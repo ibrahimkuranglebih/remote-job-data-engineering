4c4a355cfabd2f30bd50e9aeacf4b656e645ee5a
ibrahimkuranglebih <mumtazsamadikun765@gmail.com>
2026-07-08 23:38:30 +0700

    adding some base code for extracting data

--- etl/extract/extract.py
+++ etl/extract/extract.py
@@ -1,3 +1,7 @@
+import requests
+import json
+import time
+
 # selecting all country / region that available in the API response
 geo = [
     "apac", "emea", "latam", "argentina", "australia",
@@ -14,31 +18,55 @@ geo = [
     "uk", "ukraine", "usa", "vietnam"
 ]
 # total jobs per country / region with spesific industry selected
-count = 
+count = 1
 # industry field that available in the API response
 industry = [
-    "business",
-    "copywriting",
-    "healthcare",
-    "education",
-    "legal",
-    "admin-support",
-    "supporting",
-    "data-science",
-    "design-multimedia",
-    "admin",
-    "accounting-finance",
-    "hr",
-    "marketing",
-    "management",
-    "dev",
-    "seller",
-    "seo",
-    "smm",
-    "engineering",
-    "technical-support",
-    "web-app-design"
+    'admin-support', 'copywriting', 'translation-localization','supporting',
+    'technical-support', 'cybersecurity','data-science','design-multimedia',
+    'web-app-design', 'video-audio-production', 'admin', 'e-commerce',
+    'education', 'accounting-finance' ,'healthcare', 'hr', 'legal',
+    'marketing', 'business', 'seller', 'seo', 'smm', 'management',
+    'project-management', 'engineering', 'dev', 'qa-testing'
 ]
 
-# 
+# base url for the API endpoint
 
+BASE_URL = 'https://jobicy.com/api/v2/remote-jobs'
+
+all_jobs = []
+
+def extract_jobs() :
+    for g in geo:
+        for ind in industry:
+
+            params = {
+                "count": count,
+                "geo": g,
+                "industry": ind
+            }
+
+            print(f"Fetching {g} - {ind}")
+
+            try:
+                response = requests.get(BASE_URL, params=params, timeout=30)
+                response.raise_for_status()
+
+                data = response.json()
+                #print(json.dumps(data, indent=4, ensure_ascii=False))
+                
+                jobs = data.get("jobs", [])
+
+                for job in jobs:
+
+                    job["filter_geo"] = g
+                    job["filter_industry"] = ind
+
+                    all_jobs.append(job)
+
+            except Exception as e:
+                print(f"Failed {g} - {ind}: {e}")
+
+            # supaya tidak terlalu cepat request ke API
+            time.sleep(0.5)
+
+#extract_jobs()
--- sql/init.sql
+++ sql/init.sql
@@ -49,4 +49,111 @@ CREATE TABLE IF NOT EXISTS stagging.stg_job_types(
 
 CREATE SCHEMA IF NOT EXISTS mart;
 
-CREATE TABLE IF NOT EXISTS mart.
+CREATE TABLE IF NOT EXISTS mart.fact_remote_jobs(
+    job_key BIGINT PRIMARY KEY NOT NULL,
+    job_id BIGINT ,
+    company_key BIGINT,
+    location_key BIGINT,
+    date_key BIGINT,
+    salary_key BIGINT,
+    level_key BIGINT,
+    job_title VARCHAR (255),
+    job_url TEXT,
+    excerpt TEXT,
+    salary_min NUMERIC (12,2),
+    salary_max NUMERIC (12,2),
+    salary_avg NUMERIC (12,2),
+    load_date TIMESTAMP,
+
+    CONSTRAINT fk_company
+        FOREIGN KEY(company_key)
+        REFERENCES mart.dim_company(company_key),
+
+    CONSTRAINT fk_location
+        FOREIGN KEY(location_key)
+        REFERENCES mart.dim_location(location_key),
+
+    CONSTRAINT fk_level
+        FOREIGN KEY(level_key)
+        REFERENCES mart.dim_job_level(level_key),
+
+    CONSTRAINT fk_salary
+        FOREIGN KEY(salary_key)
+        REFERENCES mart.dim_salary(salary_key),
+
+    CONSTRAINT fk_date
+        FOREIGN KEY(date_key)
+        REFERENCES mart.dim_date(date_key)
+);
+
+CREATE TABLE mart.bridge_job_industry (
+
+    job_key BIGINT NOT NULL,
+    industry_key INTEGER NOT NULL,
+
+    PRIMARY KEY(job_key, industry_key),
+
+    FOREIGN KEY(job_key)
+        REFERENCES mart.fact_job_posting(job_key)
+        ON DELETE CASCADE,
+
+    FOREIGN KEY(industry_key)
+        REFERENCES mart.dim_industry(industry_key)
+);
+
+CREATE TABLE mart.bridge_job_type (
+
+    job_key BIGINT NOT NULL,
+    job_type_key INTEGER NOT NULL,
+
+    PRIMARY KEY(job_key, job_type_key),
+
+    FOREIGN KEY(job_key)
+        REFERENCES mart.fact_job_posting(job_key)
+        ON DELETE CASCADE,
+
+    FOREIGN KEY(job_type_key)
+        REFERENCES mart.dim_job_type(job_type_key)
+);
+
+CREATE TABLE IF NOT EXISTS mart.dim_company(
+    company_key BIGINT PRIMARY KEY NOT NULL,
+    company_name VARCHAR (255),
+    company_logo TEXT
+);
+
+CREATE TABLE IF NOT EXISTS mart.dim_location(
+    location_key BIGINT PRIMARY KEY NOT NULL,
+    country VARCHAR (255),
+    region VARCHAR (255)
+);
+
+CREATE TABLE IF NOT EXISTS mart.dim_salary(
+    salary_key BIGINT PRIMARY KEY NOT NULL,
+    salary_currency VARCHAR (100),
+    salary_period VARCHAR (100),
+    salary_min NUMERIC (12,2),
+    salary_max NUMERIC (12,2)
+);
+
+CREATE TABLE IF NOT EXISTS mart.dim_level(
+    level_key BIGINT PRIMARY KEY NOT NULL,
+    job_level VARCHAR (255)
+);
+
+CREATE TABLE mart.dim_industry (
+    industry_key BIGINT PRIMARY KEY,
+    industry_name VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE mart.dim_date (
+    date_key INTEGER PRIMARY KEY,
+    full_date DATE UNIQUE NOT NULL,
+    year SMALLINT,
+    quarter SMALLINT,
+    month SMALLINT,
+    month_name VARCHAR(20),
+    week SMALLINT,
+    day SMALLINT,
+    weekday VARCHAR(20)
+);
